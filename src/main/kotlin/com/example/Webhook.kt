0b82900fefd8d9612892f45ab92d4a5573e3c085
Constantine Linnick <theaspect@gmail.com>
2017-10-14 12:55:21 +0300

    Log request

--- src/main/kotlin/com/example/Webhook.kt
+++ src/main/kotlin/com/example/Webhook.kt
@@ -8,11 +8,13 @@ import javax.servlet.annotation.WebServlet
 /**
  * See https://github.com/dialogflow/dialogflow-java-client/tree/master/web/servlet
  *
- * curl -H "Content-Type: application/json; charset=utf-8" --data '{"result":{"fulfillment":{"speech":"Some speech here"}}}' "http://localhost:8080/webhook"
+ * curl -H "Content-Type: application/json; charset=utf-8" --data '{"result":{"fulfillment":{"speech":"Some speech here"}}}' "http://localhost:5000/webhook"
  */
 @WebServlet("/webhook")
 class Webhook : AIWebhookServlet() {
     override fun doWebhook(input: AIWebhookRequest, output: Fulfillment) {
-        output.displayText = "You typed:\n" + Gson().toJson(input)
+        System.out.println(Gson().toJson(input))
+        output.displayText = "Echo text"
+        output.speech = "Echo speech"
     }
 }
