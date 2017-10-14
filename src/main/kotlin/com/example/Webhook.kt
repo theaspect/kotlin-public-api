21b94c9fb5b3d228e84d088996a3833a24590d14
Constantine Linnick <theaspect@gmail.com>
2017-10-14 13:15:21 +0300

    Add insurance segment highlights

--- src/main/kotlin/com/example/Webhook.kt
+++ src/main/kotlin/com/example/Webhook.kt
@@ -3,18 +3,58 @@ package com.example
 import ai.api.model.Fulfillment
 import ai.api.web.AIWebhookServlet
 import com.google.gson.Gson
+import java.util.*
 import javax.servlet.annotation.WebServlet
 
 /**
  * See https://github.com/dialogflow/dialogflow-java-client/tree/master/web/servlet
  *
  * curl -H "Content-Type: application/json; charset=utf-8" --data '{"result":{"fulfillment":{"speech":"Some speech here"}}}' "http://localhost:5000/webhook"
+ *
+ * {
+ *  "id":"fac4f622-b0f4-49ac-aa47-ff69dc556594",
+ *  "timestamp":"Oct 14, 2017 9:56:41 AM",
+ *  "lang":"en",
+ *  "result":{
+ *      "action":"insuranceSegmentHighlights",
+ *      "score":1.0,
+ *      "source":"agent",
+ *      "parameters":{
+ *          "insuranceSegment":"property and casualty"
+ *      },
+ *      "contexts":[],
+ *      "metadata":{
+ *          "intentName":"insurance-segment-highlights",
+ *          "intentId":"97116750-b1ce-4072-bb3c-60e2196f4587",
+ *          "webhookUsed":"true"
+ *      },
+ *      "resolvedQuery":"show me pc",
+ *      "fulfillment":{
+ *          "speech":"",
+ *          "messages":[
+ *              {"speech":[""],"type":"SPEECH","platform":"DEFAULT"}
+ *          ]
+ *      },
+ *      "actionIncomplete":false
+ *  },
+ *  "status":{"code":200,"errorType":"success"},
+ *  "sessionId":"da2313b2-4c9f-1647-6913-2af28fac01df"}
  */
 @WebServlet("/webhook")
 class Webhook : AIWebhookServlet() {
+    private val random = Random()
+
     override fun doWebhook(input: AIWebhookRequest, output: Fulfillment) {
-        System.out.println(Gson().toJson(input))
-        output.displayText = "Echo text"
-        output.speech = "Echo speech"
+        if (input.result.action.equals("insuranceSegmentHighlights")) {
+            val segment = input.result.parameters["insuranceSegment"]
+
+            val clients = random.nextInt(100)
+            val premium = random.nextInt(1000)
+
+            output.speech = "In $segment you have $clients clients with $$premium premium"
+        } else {
+            System.out.println(Gson().toJson(input))
+            output.speech = "I can't understand \"${input.result.resolvedQuery}\""
+        }
     }
 }
